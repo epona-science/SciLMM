1758263f23c49b2f8bf9ad864c9e5eaeecc4299f
Ryan Smith <ryan@eponascience.com>
2020-03-22 22:10:02 -0400

    Fix reading and writing

--- scilmm/Matrices/SparseMatrixFunctions.py
+++ scilmm/Matrices/SparseMatrixFunctions.py
@@ -3,8 +3,8 @@ from scipy.sparse import save_npz, load_npz
 
 
 def save_sparse_csr(filename, array):
-    save_npz(filename, array)
+    save_npz(filename, array.tocsr())
 
 
 def load_sparse_csr(filename):
-    return load_npz(filename)
+    return load_npz(filename).tocsr()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scilmm",
-    version="0.2.0",
+    version="0.2.1",
     description="Sparse Cholesky factorIzation Linear Mixed model",
     url="https://github.com/TalShor/SciLMM.git",
     author="TalShor, kalkairis",
